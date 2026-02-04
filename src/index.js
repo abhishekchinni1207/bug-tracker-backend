529e300424be13a5291372499f2f96fd69940d1c
AbhishekChinni <chabhishek1207@gmail.com>
2026-02-04 11:36:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+import express from 'express'
+import cors from 'cors'
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Bug Tracker API running 🚀' })
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
